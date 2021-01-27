0621a9ba621e9b71eb2ec712ace11363fa7023d9
Amarnathcdj <72609355+Amarnathcdj@users.noreply.github.com>
2021-01-27 16:23:59 +0530

    Update __init__.py

--- Anie/__init__.py
+++ Anie/__init__.py
@@ -103,7 +103,7 @@ if ENV:
             "Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from LEGEND.config import Development as Config
+    from Anie.config import Development as Config
     TOKEN = Config.TOKEN
 
     try:
@@ -205,7 +205,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from LEGEND.modules.helper_funcs.handlers import (CustomCommandHandler,
+from Anie.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
